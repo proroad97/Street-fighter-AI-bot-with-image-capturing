14d4878ab1f001df279f8a6523ac663a722610ef
proroad97 <70138386+proroad97@users.noreply.github.com>
2020-12-11 22:41:54 +0200

    Update enviroment.py

--- enviroment.py
+++ enviroment.py
@@ -6,24 +6,34 @@ import pyautogui
 import cv2
 import subprocess
 
+
+
 class Enviroment():
     def __init__(path):
         self.start_game()
-        self.end_net=tf.keras.models.load_model(os.path.normcase(path["end_network"]))
+        self.end_net=tf.keras.models.load_model(os.path.normcase(path["end_network"]))#maybe a network that will understand if the player lost or win
+        self.det_net=tf.keras.models.load_model(....)#the network of object detection
 
     def start_game(self,*coord):
+        """ open the game,initialize stats and capture the "state"(take screenshot)"""
         self.wins=0
         self.loss=0
         self.health=100
         self.enemy_health=100
         self.done=False
         self.Open_game()
-        self.wind=pyautogui.getWindowsWithTitle("snes9x")[0]
-        self.wind.activate()
-        self.state=np.array(ImageGrab.grab(coord))#default: x=8,y=95,w=512,h=475
+        self.wind=pyautogui.getWindowsWithTitle("snes9x")[0]#Find the window of the game
+        self.wind.activate()#It is like we left click on the game's window,so this window become the active. With that way it is achieved sending input in the game
+        self.state=np.array(ImageGrab.grab(coord))#In my PC: x=8,y=95,w=512,h=475.change them with your coordinates
     
     def Open_game(self):
-        os.chdir(r"C:\Users\PROROAD\Desktop")
+        """ 
+        open the game and do the rights "clicks" and press the right "buttons"
+        to select the player and pause the game exactly at 99 remaing time
+        
+        Again you should pick the correct coordinates for mouse
+        """
+        os.chdir(r"C:\Users\PROROAD\Desktop")#put your game's directory
         subprocess.Popen(["snes9x-x64"],   stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         win32api.SetCursorPos((15,42))
         sleep(0.5)
@@ -54,6 +64,7 @@ class Enviroment():
 
 
     def take_state(self):
+        """we should find the coordinates of the health bar. Or with hard-code or a object detector"""
         self.image_boundarys=()
         self.state=np.array(ImageGrab.grab(self.image_boundarys))
         health_ima= cv2.cvtColor(state[:,:,:],cv2.COLOR_RGB2BGR)#taking the image with health and transform it on BGR
@@ -80,9 +91,12 @@ class Enviroment():
 
    
     def loss_win(self,state):
+        """maybe a function that will infer if the player lost or win(some times the health zeroed but the game have not ended)"""
         image=state[:,:,:]
         stats=end_net(image)
         if stats==0:return False
         elif stats==1: self.wins=+1
         else: self.loss+=1
         return True
+    
+    def reset(self):
